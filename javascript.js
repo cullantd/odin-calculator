9ce9fd511b6b55f57a5e8bca79c7e460a16b05c3
Cullan Diffee <cullandiffee@gmail.com>
2022-01-03 15:44:44 +0800

    Add code to update display on number button press

--- javascript.js
+++ javascript.js
@@ -1,3 +1,8 @@
+//here be VARIABLES
+let displayString = "";
+
+//here be FUNCTIONS
+//a and b are both numbers
 function add(a,b) {
     return a + b;
 }
@@ -13,3 +18,41 @@ function divide(a,b) {
 function multiply(a,b) {
     return a * b;
 }
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case ('+'):
+            add(a,b);
+            break;
+        case ('-'):
+            subtract(a,b);
+            break;
+        case ('/'):
+            divide(a,b);
+            break;
+        case ('*'):
+            multiply(a,b);
+            break;
+    }
+}
+
+function appendToDisplay(number) {
+    displayString = displayString.concat(number);
+    updateDisplay(displayString);
+}
+
+function updateDisplay(stringToReplace) {
+    document.getElementById("display").textContent = stringToReplace;
+}
+
+//here be MAIN
+updateDisplay(displayString);
+
+//if button w/ class number-button is clicked, append to display
+const numberButtons = document.querySelectorAll('.number-button')
+numberButtons.forEach((numberButton) => {
+    numberButton.addEventListener('click', () => {
+        appendToDisplay(numberButton.id);
+    });
+});
+
